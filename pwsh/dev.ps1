4339c15ce4fa2eedc3d6d6098236d1132aa6adfe
Vinicius Cardoso <vinicius.cardoso@sapiensia.com>
2024-03-12 18:02:27 -0300

    Incluindo gerador de cpf

--- pwsh/dev.ps1
+++ pwsh/dev.ps1
@@ -30,13 +30,63 @@ function Invoke-AndroidEmulator {
   }
 }
 
+# Gerar cpfs aleatórios
+function GenerateRandomChunk {
+  return (Get-Random -Minimum 0 -Maximum 999).ToString('000');
+}
+
+function CalculateCheckDigit {
+  param(
+    [Parameter(Mandatory=$true)][string] $chunk1,
+    [Parameter(Mandatory=$true)][string] $chunk2,
+    [Parameter(Mandatory=$true)][string] $chunk3,
+    [Parameter()][string] $vd1
+  )
+  
+  $chunk4 = !$vd1 ? "" : $vd1;
+  [int[]]$numbers = "$chunk1$chunk2$chunk3$chunk4".ToCharArray() | % {iex $_};
+
+  $sum = 0;
+  $index = 0;
+  $start = !$vd1 ? 10 : 11;
+
+  for ($num = $start; $num -ge 2; $num--) {
+    $sum += [int]::parse($numbers[$index]) * $num;
+    $index++;
+  }
+
+  $remainder = $sum % 11;
+  return $remainder -lt 2 ? 0 : 11 - $remainder;
+}
+
+function GenerateCpf {
+  $chunk1 = GenerateRandomChunk;
+  $chunk2 = GenerateRandomChunk;
+  $chunk3 = GenerateRandomChunk;
+  $vd1 = CalculateCheckDigit $chunk1 $chunk2 $chunk3;
+  $vd2 = CalculateCheckDigit $chunk1 $chunk2 $chunk3 $vd1;
+
+  write-host "$chunk1.$chunk2.$chunk3-$vd1$vd2"
+}
+
+function Invoke-CreateCpf {
+  param([Parameter()][int] $count)
+
+  $count = $count -eq 0 ? 1 : $count;
+
+  for($i=0; $i -lt $count; $i++) {
+    GenerateCpf
+  }
+}
+
 function Invoke-GetAllFunctionsDev {
     $aliases = New-Object System.Collections.ArrayList;
     $aliases.AddRange((
         [Tuple]::Create("guid", "Invoke-GenerateNewGuid", "Gerar N guids aleatórias"),
         [Tuple]::Create("grep", "findstr", "Encontrar strings em fluxos de dados"),
         [Tuple]::Create("droid", "Invoke-AndroidEmulator", "Rodar emulador android"),
-        [Tuple]::Create("shistory", "Invoke-PowershellHistory", "Histórico de comandos do powershell")
+        [Tuple]::Create("shistory", "Invoke-PowershellHistory", "Histórico de comandos do powershell"),
+        [Tuple]::Create("cpf", "Invoke-CreateCpf", "Cria N CPFs aleatórios válidos")
     ));
     return $aliases
 }
